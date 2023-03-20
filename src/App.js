29f969a393aefa45cbce1e7990a957e2bf842741
13angs <13angz.um@gmail.com>
2023-03-20 07:41:18 +0000

    add tailwind-css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import('./index.css');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='border'>
+        <nav className='container mx-auto py-5'>
+          <h1 className='text-xl font-black'>HowToBase64</h1>
+        </nav>
+      </div>
+      <div className='container mx-auto'>
+        <div className='flex'>
+          {/* drawer */}
+          <div className='flex-initial w-64 border-x border-b h-full px-3 py-3'>
+            <div>
+              <input className='w-full border'></input>
+            </div>
+            <div className='w-full py-3'>
+              <button>Hello world</button>
+            </div>
+          </div>
+          {/* content */}
+          <div className='flex-1'>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
