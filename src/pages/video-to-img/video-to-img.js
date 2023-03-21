9ac1145ee4ce0927095bfeeaa41a32c192ccec3b
13angs <13angz.um@gmail.com>
2023-03-21 07:21:19 +0000

    convert canvas to base64 img

--- src/pages/video-to-img/video-to-img.js
+++ src/pages/video-to-img/video-to-img.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export default function VideoToImg(){
+export default function VideoToImg() {
     const [file, setFile] = React.useState({
         imgFile: null,
         imgFileType: null,
@@ -8,54 +8,59 @@ export default function VideoToImg(){
         videoFileType: null
     });
 
-    const readAsBase64 = (e) => {
-        const base64String = e.target.result.replace("data:", "")
-        .replace(/^.+,/, "");
-        
-        // if(base64String)
-        // {
-        //     textarea.textContent = base64String;
-        // }
+    const readAsBase64 = (base64Data) => {
+        return base64Data.replace("data:", "")
+            .replace(/^.+,/, "");
+    }
 
+    const handleSetFile = (e) => {
+        const base64String = readAsBase64(e.target.result);
         setFile(prev => ({
             ...prev,
             videoFile: base64String
         }))
     }
+
     const handleUpload = (e) => {
         const file = e.target.files[0];
         setFile(prev => ({
-          ...prev,
-          videoFileType: file.type
+            ...prev,
+            videoFileType: file.type
         }))
         const reader = new FileReader();
-        reader.addEventListener('load', readAsBase64);
+        reader.addEventListener('load', handleSetFile);
         // reader.readAsText(file);
         reader.readAsDataURL(file);
-      }
-    
-      const handleConvertVidToImg = () => {
-          // Get a reference to the video element
-          const video = document.getElementById('my-video');
-      
-          // Get a reference to the canvas element
-          const canvas = document.getElementById('my-canvas');
-          const ctx = canvas.getContext('2d');
-      
-          // Wait for the video to be ready to play
-          // Set the canvas dimensions to match the video dimensions
-          console.log(video.getBoundingClientRect())
-          const {width, height} = video.getBoundingClientRect();
-          canvas.width = width;
-          canvas.height = height;
-      
+    }
+
+    const handleConvertVidToImg = () => {
+        // Get a reference to the video element
+        const video = document.getElementById('my-video');
+
+        // Get a reference to the canvas element
+        const canvas = document.getElementById('my-canvas');
+        const ctx = canvas.getContext('2d');
+
+        // Wait for the video to be ready to play
+        // Set the canvas dimensions to match the video dimensions
+        const { width, height } = video.getBoundingClientRect();
+        canvas.width = width;
+        canvas.height = height;
+
         //   // Draw the current video frame onto the canvas
-        console.log(video)
-            if(video)
-            {
-                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-            }
-      
+        if (video) {
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        }
+
+        const imgData = canvas.toDataURL();
+        const imgType = imgData.split(':')[1].split(';')[0];
+        const b64data = readAsBase64(imgData)
+        setFile(prev => ({
+            ...prev,
+            imgFile: b64data,
+            imgFileType: imgType
+        }));
+
         //   // Convert the canvas to a data URL and display it as an image
         //   const image = new Image();
         //   image.src = canvas.toDataURL();
@@ -63,14 +68,17 @@ export default function VideoToImg(){
         //   document.body.appendChild(image);
         //   video.addEventListener('canplay', () => {
         //   });
-      }
+    }
+
+    console.log(file.imgFile)
+    console.log(file.imgFileType)
 
     return (
         <div>
             <h2 className="text-lg font-extrabold">
                 Converting video to image
             </h2>
-            <div className="border-b my-2"/>
+            <div className="border-b my-2" />
             <div className="grid grid-cols-2 gap-4">
                 <form className="flex justify-center items-center space-x-6 border border-dotted py-11">
                     <label className="block">
@@ -86,20 +94,38 @@ export default function VideoToImg(){
                 </form>
                 <div>
                     {
-                        file.videoFile ?  
-                        <>
-                            <div className='mb-3'>
-                                <video id="my-video" title="base64-video" className="w-full aspect-video" src={`data:${file.videoFileType};base64,${file.videoFile}`} controls></video>
-                            </div>
-                            <div>
-                                {/* <textarea className='text-black w-full text-sm h-20 bg-slate-100 p-2' value={file.videoFile ? file.videoFile : ""} /> */}
-                            </div>
-                            <button onClick={handleConvertVidToImg}>Convert to image</button>
-                            <div>
-                                <canvas id="my-canvas"/>
-                            </div>
-                        </> :
-                        <div className='flex justify-center items-center py-11'>No video being uploaded</div>
+                        file.videoFile ?
+                            <>
+                                <div className='mb-3'>
+                                    <video id="my-video" title="base64-video" className="w-full aspect-video" src={`data:${file.videoFileType};base64,${file.videoFile}`} controls></video>
+                                    {/* <code className='text-slate-500 truncate'>
+                                        {file.videoFile ? file.videoFile : ""}
+                                    </code> */}
+                                    <span>File type: {file.videoFileType ? file.videoFileType : ""}</span>
+                                    <textarea className='text-black w-full text-sm h-20 bg-slate-100 p-2 mt-2 overflow-x-hidden' value={file.videoFile ? file.videoFile : ""} />
+                                </div>
+                                <div>
+                                    {/* <textarea className='text-black w-full text-sm h-20 bg-slate-100 p-2' value={file.videoFile ? file.videoFile : ""} /> */}
+                                </div>
+                                <button className='block w-full
+                                                    mb-3 py-2 px-4
+                                                    rounded-full border-0
+                                                    text-sm font-semibold
+                                                    bg-violet-50 text-violet-700
+                                                    hover:bg-violet-100' 
+                                    onClick={handleConvertVidToImg}>Convert to image</button>
+                                <div>
+                                    <canvas hidden id="my-canvas" className='w-full aspect-video' />
+                                    {file.imgFile && 
+                                        <>
+                                            <img className='w-full' src={`data:${file.imgFileType};base64,${file.imgFile}`} alt="" />
+                                            <span>File type: {file.imgFileType ? file.imgFileType : ""}</span>
+                                            <textarea className='text-black w-full text-sm h-20 bg-slate-100 p-2 mt-2 overflow-x-hidden' value={file.imgFile ? file.imgFile : ""} />
+                                        </>
+                                    }
+                                </div>
+                            </> :
+                            <div className='flex justify-center items-center py-11'>No video being uploaded</div>
                     }
                 </div>
             </div>
